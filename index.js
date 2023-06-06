5c9b32db9339a8cae1d20f30a8e86c257ce54729
TJ <teejaynam@gmail.com>
2023-06-06 19:53:05 -0400

    added account class, updated functions to use account class and added deposit function

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-let balance = 500.00;
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+
+}
 
 class Withdrawal {
 
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
+  }
+
+}
+
+class Deposit {
+
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  commit() {
+    this.account.balance += this.amount;
   }
 
 }
@@ -18,12 +39,20 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log('Balance:', myAccount.balance);
+
+
